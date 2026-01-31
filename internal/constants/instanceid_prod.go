431156d9485d0bdeb9548647f335b9d6ea950d6d
Ansar Smagul <ansar@anfragment.net>
2026-01-31 22:22:54 +0500

    internal/constants,tasks/build: assign random InstanceID during production builds (#576)
    * internal/constants: make InstanceID var to allow overriding via ldflags; add explanatory comment
    
    * tasks/build: add constants.InstanceID override by generating a random UUID

--- internal/constants/instanceid_prod.go
+++ internal/constants/instanceid_prod.go
@@ -2,4 +2,8 @@
 
 package constants
 
-const InstanceID = "a7bad8a9-cadb-4ae9-86b3-2e9e81049cb8"
+// InstanceID is the unique identifier for the application instance.
+// This should be overridden at build time using ldflags:
+// -ldflags "-X 'github.com/ZenPrivacy/zen-desktop/internal/constants.InstanceID=<uuid>'"
+// If not overridden, falls back to a default UUID.
+var InstanceID = "a7bad8a9-cadb-4ae9-86b3-2e9e81049cb8"
